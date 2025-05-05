2d86585b4f0af64988b00034d84e9f8cf348450b
Fabian Viöl <fv@enqt.de>
2025-05-05 09:57:48 +0800

    Add support for changing interface type

--- src/iface/handle.rs
+++ src/iface/handle.rs
@@ -1,8 +1,30 @@
 // SPDX-License-Identifier: MIT
 
-use crate::{Nl80211Handle, Nl80211InterfaceGetRequest};
+use crate::{
+    Nl80211Attr, Nl80211AttrsBuilder, Nl80211Handle, Nl80211InterfaceGetRequest,
+};
+
+use super::{Nl80211InterfaceSetRequest, Nl80211InterfaceType};
 
 pub struct Nl80211InterfaceHandle(Nl80211Handle);
+
+#[derive(Debug)]
+pub struct Nl80211Interface;
+
+impl Nl80211Interface {
+    /// Change properties of the interface
+    pub fn new(if_index: u32) -> Nl80211AttrsBuilder<Self> {
+        Nl80211AttrsBuilder::<Self>::new().if_index(if_index)
+    }
+}
+
+impl Nl80211AttrsBuilder<Nl80211Interface> {
+    /// Change the interface type
+    /// (equivalent to `iw dev type <type>`)
+    pub fn interface_type(self, r#type: Nl80211InterfaceType) -> Self {
+        self.replace(Nl80211Attr::IfType(r#type))
+    }
+}
 
 impl Nl80211InterfaceHandle {
     pub fn new(handle: Nl80211Handle) -> Self {
@@ -13,5 +35,13 @@ impl Nl80211InterfaceHandle {
     /// (equivalent to `iw dev`)
     pub fn get(&mut self) -> Nl80211InterfaceGetRequest {
         Nl80211InterfaceGetRequest::new(self.0.clone())
+    }
+
+    /// Set wireless interfaces attributes
+    pub fn set(
+        &mut self,
+        attributes: Vec<Nl80211Attr>,
+    ) -> Nl80211InterfaceSetRequest {
+        Nl80211InterfaceSetRequest::new(self.0.clone(), attributes)
     }
 }
